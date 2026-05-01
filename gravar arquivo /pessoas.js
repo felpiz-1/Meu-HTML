460f27e13992d0e60ec47f7f29d92fd2e5646ffd
felpiz-1 <rochadeandradef@gmail.com>
2026-04-30 22:04:36 -0300

    30/04

---
+++ gravar arquivo /pessoas.js
@@ -0,0 +1,8 @@
+class Pessoa {
+    constructor(nome, idade, curso, modulo){
+        this.nome = nome;
+        this.idade = parseInt(idade);
+        this.curso = curso;
+        this.modulo = modulo;
+    }
+}
